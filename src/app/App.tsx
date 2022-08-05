a048688f0ea6e45489ff6e07d9de10e292d792d2
Bruno Calasans <83395524+Bruno-Calasans@users.noreply.github.com>
2022-08-05 14:48:31 -0300

    feat: possível calendário

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,18 @@
+
+// react
+import { Route, Routes } from "react-router-dom";
+
+// estilos
 import { GlobalStyle } from "./App.styles";
+
+// componentes
 import { LoginCadastro } from "./pages/LoginCadastro";
 import { Listagem } from './pages/ListagemProdutos'
 import { CreateProductModal } from "./components/CreateProduct";
+import Calendario from "./pages/Calendario";
+
+// contextos
 import { CreateProdcutProvider } from "./contexts/Products/CreateProductProvider ";
-import { Route, Routes } from "react-router-dom";
 import { RequireAuth } from "./contexts/Auth/RequireAuth";
 
 export const App = () => {
@@ -23,8 +32,12 @@ export const App = () => {
           element={<CreateProductModal isOpen={true}/>} />
 
           <Route 
-          path="/list" 
+          path="/" 
           element={<Listagem />} />
+
+          <Route 
+          path="/calendar" 
+          element={<Calendario />} />
 
         </Routes>
     </CreateProdcutProvider>
