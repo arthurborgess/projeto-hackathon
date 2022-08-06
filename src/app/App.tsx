474f8c1831ff74243dfd7549b92a844ead546367
Bruno Calasans <83395524+Bruno-Calasans@users.noreply.github.com>
2022-08-06 20:03:27 -0300

    resolução de conflitos

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,27 +9,24 @@ import { GlobalStyle } from "./App.styles";
 import { LoginCadastro } from "./pages/LoginCadastro";
 import { Listagem } from './pages/ListagemProdutos'
 import { CreateProductModal } from "./components/CreateProduct";
-<<<<<<< HEAD
-=======
+
 import Calendario from "./pages/Calendario";
 import EditProduct from "./components/EditProduct"
->>>>>>> d1cdfda6807de0072561b78471b20d31fe8eb87c
-
 // contextos
 import { CreateProdcutProvider } from "./contexts/Products/CreateProductProvider ";
 import { RequireAuth } from "./contexts/Auth/RequireAuth";
-<<<<<<< HEAD
+
 import { Calendar } from "./pages/Calendar";
 import { ProdcutProvider } from "./contexts/Products/ProdcutsProvider";
-=======
+
 import { ListaCompleta } from "./pages/ListaCompleta";
 
->>>>>>> d1cdfda6807de0072561b78471b20d31fe8eb87c
 
 export const App = () => {
   return (
+    <>
     <CreateProdcutProvider>
-<<<<<<< HEAD
+
       <ProdcutProvider>
         <GlobalStyle/>
           <Routes>
@@ -40,7 +37,7 @@ export const App = () => {
           </Routes>
         </ProdcutProvider>
       </CreateProdcutProvider>
-=======
+
       <GlobalStyle />
       
       <Routes>
@@ -57,7 +54,6 @@ export const App = () => {
         <Route path="/edit" element={<RequireAuth><EditProduct /></RequireAuth>} />
 
       </Routes>
-    </CreateProdcutProvider>
->>>>>>> d1cdfda6807de0072561b78471b20d31fe8eb87c
+    </>
   );
 }
