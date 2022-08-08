26f0e1533299f49de90c2b533964fe2fdff39b72
Arthur Borges <arthurborges@ufu.br>
2022-08-08 16:01:10 -0300

    mudanças no input CPF

--- src/app/components/InputCpf/index.tsx
+++ src/app/components/InputCpf/index.tsx
@@ -10,18 +10,26 @@ export const InputCpf = ({ cpf, setCpf }: Props) => {
 
         if ((/[0-9]+/g).test(event.key) && event.target.value.length <= 13) {
             event.currentTarget.value += event.key;
+
+            if (event.target.value.length === 3) {
+                event.currentTarget.value += '.';
+            }
+            if (event.target.value.length === 7) {
+                event.currentTarget.value += '.';
+            }
+            if (event.target.value.length === 11) {
+                event.currentTarget.value += '-';
+            }
+        } else if (event.key === 'Backspace') {
+            let currentValue = cpf;
+            currentValue = currentValue.slice(0, -1);
+            setCpf(currentValue);
+            return;
         }
 
-        if (event.target.value.length === 3) {
-            event.currentTarget.value += '.';
-        }
-        if (event.target.value.length === 7) {
-            event.currentTarget.value += '.';
-        }
-        if (event.target.value.length === 11) {
-            event.currentTarget.value += '-';
-        }
         setCpf(event.currentTarget.value);
+
+        console.log(cpf)
     }
 
     return (
@@ -29,10 +37,13 @@ export const InputCpf = ({ cpf, setCpf }: Props) => {
             id="cpf-input"
             type="text"
             placeholder="CPF"
-            onKeyPress={cpfValidate}
+            value={cpf}
+            onChange={e => setCpf(e.target.value)}
+            onKeyDown={cpfValidate}
             onPaste={e => { e.preventDefault() }}
             required
             minLength={14}
+            maxLength={14}
         />
     );
 }
