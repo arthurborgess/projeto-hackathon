ba1c2c7bd933ea1b179926f2c5a17df7b19e9699
Renato Sousa <94645489+SousaExm@users.noreply.github.com>
2022-08-07 23:48:06 -0300

    alteracao no botao de criar produto

--- src/app/components/CreateProduct/styled.ts
+++ src/app/components/CreateProduct/styled.ts
@@ -69,8 +69,10 @@ export const Cancel = styled.button`
   outline: none;
   border-radius: 8px;
   border: 1px solid #1a5fff;
+  text-align: center;
   width: 100px;
   height: 32px;
+  line-height: 32px;
   
   &:hover {
     cursor: pointer;
@@ -80,8 +82,10 @@ export const Create = styled.button`
   outline: none;
   border: none;
 
+  text-align: center;
   width: 100px;
   height: 32px;
+  line-height: 32px;
   color: #ffffff;
   border-radius: 8px;
   background-color: #1a5fff;
